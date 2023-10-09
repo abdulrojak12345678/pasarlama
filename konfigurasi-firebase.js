21a6d7c277a074a3f35d7beee4153543d85326cd
stondibrahim204@gmail.com <stondibrahim204@gmail.com>
2023-10-09 11:01:33 +0700

    [WIP] menambah konfigurasi firebase

---
+++ konfigurasi-firebase.js
@@ -0,0 +1,9 @@
+export const konfigurasiFirebase = {
+  apiKey: "AIzaSyCGdCQI2D46xXf8OkhL4YziDWMHyrGEfjM",
+  authDomain: "ibrahim-235d7.firebaseapp.com",
+  projectId: "ibrahim-235d7",
+  storageBucket: "ibrahim-235d7.appspot.com",
+  messagingSenderId: "935701641604",
+  appId: "1:935701641604:web:ec49facd57c9a56445c5d1",
+  measurementId: "G-0FQ7VSDM4E"
+};
